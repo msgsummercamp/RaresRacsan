e6586594fc168df739b744d46aa35b49e1f2c1ca
RaresRacsan <rares.racsan@gmail.com>
2025-07-16 11:00:21 +0300

    maven first task added

---
+++ day1BE/maven/src/test/java/org/example/MainTest.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void main() {
+        assert(true); // This is a placeholder test
+    }
+}

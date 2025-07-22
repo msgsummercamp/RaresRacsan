c04ac4b0df1ec2097b4b27a7f43b0fa28891b293
RaresRacsan <rares.racsan@gmail.com>
2025-07-22 11:15:44 +0300

    - changed any type in ts file - css added border-top-color - removed js file

--- day1FE/ts/script.ts
+++ day1FE/ts/script.ts
@@ -1,15 +1,19 @@
 const fetchUrl: string = "https://dog.ceo/api/breeds/image/random";
-const dogImage: HTMLImageElement = document.getElementById("dog-image") as HTMLImageElement;
-const loader: HTMLDivElement = document.getElementById("loader") as HTMLDivElement;
+const dogImage: HTMLImageElement = document.getElementById(
+  "dog-image"
+) as HTMLImageElement;
+const loader: HTMLDivElement = document.getElementById(
+  "loader"
+) as HTMLDivElement;
 
 async function showDog(): Promise<void> {
-    try {
-        const response: Promise<Response> = fetch(fetchUrl);
-        const data: any = await (await response).json();
-        dogImage.src = data.message;
-        dogImage.hidden = false;
-        loader.hidden = true;
-    } catch (error) {
-        console.error("Error fetching dog image:", error);
-    }
+  try {
+    const response: Response = await fetch(fetchUrl);
+    const data: { message: string } = await response.json();
+    dogImage.src = data.message;
+    dogImage.hidden = false;
+    loader.hidden = true;
+  } catch (error) {
+    console.error("Error fetching dog image:", error);
+  }
 }

b50a46dfeb0114d3c7b42a74c8029e16bf191262
RaresRacsan <rares.racsan@gmail.com>
2025-07-24 13:09:27 +0300

    - created Routes for Home, Login, PagNotFound

---
+++ day2FE/my-app/src/app/home/home.component.ts
@@ -0,0 +1,56 @@
+import { Component, inject, signal, WritableSignal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButton } from '@angular/material/button';
+import { HttpClient } from '@angular/common/http';
+import { AuthDirective } from '../auth.directive';
+
+// response type for the showRandomDog method
+type DogResponse = {
+  message: string;
+  status: string;
+};
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, MatButton, AuthDirective],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+})
+export class HomeComponent {
+  // hardcoded logged-in state
+  public readonly loggedIn: WritableSignal<boolean> = signal(false);
+  private readonly http = inject(HttpClient);
+  public readonly data = signal('');
+
+  // hardcoded logged-in state
+  public logIn(): void {
+    this.loggedIn.set(true);
+  }
+
+  public logOut(): void {
+    this.loggedIn.set(false);
+  }
+
+  async showRandomDog() {
+    // the url and the image element
+    const fetchUrl: string = 'https://dog.ceo/api/breeds/image/random';
+    const dogImage: HTMLImageElement = document.getElementById(
+      'dogImage'
+    ) as HTMLImageElement;
+
+    // fetch the data and update the image source
+    this.http.get<DogResponse>(fetchUrl).subscribe({
+      next: (response) => {
+        if (response.status !== 'success') {
+          console.error('Failed to fetch dog image');
+          return;
+        }
+        this.data.set(response.message);
+        dogImage.src = this.data();
+        dogImage.hidden = false;
+      },
+      error: (err) => console.log('Error: ' + err),
+    });
+  }
+}

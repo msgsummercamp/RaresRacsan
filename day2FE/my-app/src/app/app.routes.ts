082c85480f960474301082382daf85e95ace766e
RaresRacsan <rares.racsan@gmail.com>
2025-07-25 09:25:37 +0300

    - solved interceptor error which wont let the logged in user show random dogs

--- day2FE/my-app/src/app/app.routes.ts
+++ day2FE/my-app/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { HomeComponent } from './home/home.component';
 import { RandomDogComponent } from './random-dog/random-dog.component';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   {
@@ -21,6 +22,7 @@ export const routes: Routes = [
   {
     path: 'randomDog',
     component: RandomDogComponent,
+    canActivate: [authGuard],
   },
   {
     path: '**',

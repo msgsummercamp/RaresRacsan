1bd0c6522c21ea4ffd8a6fae7494e826f8d2e6ae
RaresRacsan <rares.racsan@gmail.com>
2025-07-25 11:37:18 +0300

    - login form created - logout button added on toolbar

--- day2FE/my-app/src/app/app.component.ts
+++ day2FE/my-app/src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatToolbar } from '@angular/material/toolbar';
 import { MatButton } from '@angular/material/button';
 import { RouterOutlet } from '@angular/router';
 import { RouterLink } from '@angular/router';
 import { RouterLinkActive } from '@angular/router';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -12,4 +13,9 @@ import { RouterLinkActive } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+  private readonly authService = inject(AuthService);
+  public logout(): void {
+    this.authService.logOut();
+  }
+}
--- day2FE/my-app/src/app/services/auth.service.ts
+++ day2FE/my-app/src/app/services/auth.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, signal } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
@@ -6,9 +7,11 @@ import { Injectable, signal } from '@angular/core';
 export class AuthService {
   private readonly _loggedIn = signal<boolean>(false);
   public readonly loggedIn = this._loggedIn.asReadonly();
+  constructor(private router: Router) {}
 
   public logIn(): void {
     this._loggedIn.set(true);
+    this.router.navigate(['/home']);
   }
 
   public logOut(): void {
